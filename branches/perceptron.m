0c13905d3668684afebada63753ae75a3f91986b
xiejue.pku@gmail.com <xiejue.pku@gmail.com@bf9fa20d-b9d8-badb-6b62-2e238122929b>
2013-04-19 16:58:25 +0000

    perceptron function

---
+++ branches/perceptron.m
@@ -0,0 +1,19 @@
+function w = perceptron(train1, train2)
+
+y1 = [ones(size(train1, 1), 1), train1];
+y2 = [ones(size(train2, 1), 1), train2];
+y = [y1; -y2];
+
+eta = 0.1;
+w = 0.1 * rand(size(y, 2), 1);
+
+for i = 1:1000
+	ind = find(y * w <= 0);
+	if isempty(ind), break, end;
+	corr = sum(y(ind, :), 1);
+	w = w + eta * corr';	
+end
+
+end
+
+
